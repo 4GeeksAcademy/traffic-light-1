36999f52d8e20d82b03ea9ec76d89c0041619a85
cristiang83 <156969890+cristiang83@users.noreply.github.com>
2024-03-11 23:35:46 +0000

    FIn

--- src/componentes/TrafficLights.jsx
+++ src/componentes/TrafficLights.jsx
@@ -1,9 +1,27 @@
-export function TrafficLights () {
-    return (
-        <div className="traffic">
-            <div className="red"/>
-            <div className="yellow"/>
-            <div className="green"/>
-        </div>
-    )
+import { useState } from "react";
+import clsx from "clsx";
+
+export function TrafficLights() {
+  const [light, setLight] = useState("red");
+
+  const handlerChangeLight = (l) => () => {
+    setLight(l);
+  };
+
+  return (
+    <div className="traffic">
+      <div
+        className={clsx("red", light === "red" && "glow")}
+        onClick={handlerChangeLight("red")}
+      />
+      <div
+        className={clsx("yellow", light === "yellow" && "glow")}
+        onClick={handlerChangeLight("yellow")}
+      />
+      <div
+        className={clsx("green", light === "green" && "glow")}
+        onClick={handlerChangeLight("green")}
+      />
+    </div>
+  );
 }
